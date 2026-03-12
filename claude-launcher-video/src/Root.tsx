8e5bd89313b2433e6a70b11fefea3cf61e495756
lucasaugustodev <lucas@algoricca.com>
2026-03-11 22:31:01 -0300

    feat: add new video compositions (Imobiliaria, Ecommerce, Dropshipping)

--- claude-launcher-video/src/Root.tsx
+++ claude-launcher-video/src/Root.tsx
@@ -1,15 +1,44 @@
 import { Composition } from "remotion";
 import { ClaudeLauncherVideo } from "./Video";
+import { DemoImobiliaria } from "./DemoImobiliaria";
+import { DemoEcommerce } from "./DemoEcommerce";
+import { DemoDropshipping } from "./DemoDropshipping";
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <Composition
-      id="ClaudeLauncherDemo"
-      component={ClaudeLauncherVideo}
-      durationInFrames={450}
-      fps={30}
-      width={1920}
-      height={1080}
-    />
+    <>
+      <Composition
+        id="ClaudeLauncherDemo"
+        component={ClaudeLauncherVideo}
+        durationInFrames={450}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="Imobiliaria"
+        component={DemoImobiliaria}
+        durationInFrames={270}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="Ecommerce"
+        component={DemoEcommerce}
+        durationInFrames={270}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="Dropshipping"
+        component={DemoDropshipping}
+        durationInFrames={270}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+    </>
   );
 };
