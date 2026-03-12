fa9ddfe9172a3345afc9e084e64cfbe9fd59e39c
lucasaugustodev <lucas@algoricca.com>
2026-03-11 23:22:13 -0300

    feat: add EcommerceV3 and EcommerceReels compositions to RemotionRoot

--- claude-launcher-video/src/Root.tsx
+++ claude-launcher-video/src/Root.tsx
@@ -3,6 +3,7 @@ import { ClaudeLauncherVideo } from "./Video";
 import { DemoImobiliaria } from "./DemoImobiliaria";
 import { DemoEcommerce } from "./DemoEcommerce";
 import { DemoDropshipping } from "./DemoDropshipping";
+import { EcommerceH, EcommerceV } from "./DemoEcommerceV3";
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -38,6 +39,22 @@ export const RemotionRoot: React.FC = () => {
         fps={30}
         width={1920}
         height={1080}
+      />
+      <Composition
+        id="EcommerceV3"
+        component={EcommerceH}
+        durationInFrames={285}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="EcommerceReels"
+        component={EcommerceV}
+        durationInFrames={285}
+        fps={30}
+        width={1080}
+        height={1920}
       />
     </>
   );
